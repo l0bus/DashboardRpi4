b75eae88992cf4aa3019b229fa622a533cd6f798
berty <lucho.2012.tandil@gmail.com>
2021-09-11 18:29:04 -0300

    Se crea migracion y modelo para tabla de equipos, en esta tabla se creará un registro por cada equipo nuevo sobre el cual se recibiràn logs

--- back/dashboardAPI/models.py
+++ back/dashboardAPI/models.py
@@ -8,3 +8,9 @@ class TipoEquipos(models.Model):
     cod = models.CharField(max_length=255)
     descripcion = models.CharField(max_length=255)
     nombre = models.CharField(max_length=255)
+
+class Equipos(models.Model):
+    cod = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+    tipo = models.ForeignKey(TipoEquipos, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField()
