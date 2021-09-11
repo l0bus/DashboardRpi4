513e3c77ba1a138afca304805eeae8c9e3725074
berty <lucho.2012.tandil@gmail.com>
2021-09-11 18:12:51 -0300

    Se agrega modelo y migraciones de tabla tipoequipos, que se encargará de almacenar los diferentes tipos de quipos

--- back/dashboardAPI/models.py
+++ back/dashboardAPI/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class CamposLog(models.Model):
     descripcion = models.CharField(max_length=255)
     cod = models.CharField(max_length=255)
+
+class TipoEquipos(models.Model):
+    cod = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+    nombre = models.CharField(max_length=255)
