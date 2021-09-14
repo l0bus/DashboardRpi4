a3ba99df762f1f2540922d37fa4b0a57db6d903a
berty <lucho.2012.tandil@gmail.com>
2021-09-14 02:21:39 -0300

    Se agrega campo extrapara especificar el nombre del archivo cargada, para evitar repeticiones en la carga de registro

--- back/dashboardAPI/models.py
+++ back/dashboardAPI/models.py
@@ -20,6 +20,7 @@ class Equipos(models.Model):
 
 class LogEquipoReg(models.Model):
     id = models.AutoField(primary_key=True)
+    file_path = models.CharField(max_length=512)
     id_equipo = models.ForeignKey(Equipos, on_delete=models.DO_NOTHING)
     fecha_registro = models.DateTimeField()
 
